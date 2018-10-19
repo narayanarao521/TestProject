59b14802db4e9ce579605eaf5422b911ef98e01b
NarayanaRao <NarayanaRao@Lenovo-PC>
2018-10-19 22:10:28 +0530

    Second commit

---
+++ TestProject/src/org/narayanarao/FirstProgram.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.narayanarao;
+
+/**
+ * @author NarayanaRao
+ *
+ */
+public class FirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
